d6a5825a488350914137341c4b5bc5f6b3ac151d
trir2d2 <34526713+trir2d2@users.noreply.github.com>
2018-03-23 15:50:14 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-echo "Initialisation des ressources""
-echo -ne '#####                     (33%)\r'
-sleep 1
-echo -ne '#############             (66%)\r'
-sleep 1
-echo -ne '#######################   (100%)\r'
-echo -ne '\n'
 sudo apt-get update
 echo "Mise à jour des paquets"
 sudo apt-get upgrade
-
 echo -e "\e[43mInstallation de HTOP\e[0m"
 sudo apt-get --yes --force-yes install htop
-
-echo -e "\e[5m \e[102mCHECK\e[0m"
-echo -e "\e[43mAlias sudo\e[0m"
 alias s='sudo'
-echo -e "\e[5m \e[102mCHECK\e[0m"
