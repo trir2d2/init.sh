4429122db9f7587f38332b26464ef0ff120a742c
trir2d2 <34526713+trir2d2@users.noreply.github.com>
2018-04-01 23:12:23 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ sudo apt-get upgrade
 echo -e "\e[43mInstallation de HTOP\e[0m"
 sudo apt-get --yes --force-yes install htop
 alias s='sudo'
+sudo apt-get install --yes --force-yes curl
+curl -sSL https://get.docker.com/ | sh
